662756cb94943220e9543c152e6145e88f9de4db
André Mazili <maziliandre@gmail.com>
2025-03-22 15:39:55 -0300

    Button presses and Clear functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
+function createCalc(){
+    return {
+        display: document.querySelector('#display'),
+        btnClear: document.querySelector('.btn-clear'),
+        
+        
+        initialize() {
+            this.clickMethod();
+        },
 
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        clickMethod() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+                if (el.classList.contains('btn-num')){
+                    this.btnToDisplay(el.innerText);
+                }
+                if (el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+            });
+        },
+
+        btnToDisplay(value){
+            this.display.value += value;
+        },
+    };
+}
+
+const calculator = createCalc();
+calculator.initialize();
