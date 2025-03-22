e356154b95325a0d776e5dc8fb167a540ad757f4
André Mazili <maziliandre@gmail.com>
2025-03-22 16:33:44 -0300

    Updated to constructor function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,52 @@
-function createCalc(){
-    return {
-        display: document.querySelector('#display'),
-        
-        
-        initialize() {
-            this.clickMethod();
-        },
+function Calc(display){
+    this.display = display;
 
-        clearDisplay(){
-            this.display.value = '';
-        },
+    this.initialize = function(){
+        clickMethod();
+    }
 
-        deleteOne(){
-            this.display.value = this.display.value.slice(0, -1);
-        },
-
-        doesEquation(){
-            let equation = this.display.value;
-            try{
-                equation = eval(equation);
-                if(!equation){
-                    alert('Invalid calculation');
-                    return;
-                }
-                this.display.value = equation;
-            }
-            catch(e) {
+    const btnToDisplay = function(value){
+        this.display.value += value;
+    }
+    const clearDisplay = function(){
+        this.display.value = '';
+    }
+    const deleteOne = function(){
+        this.display.value = this.display.value.slice(0, -1);
+    }
+    const doesEquation = function(){
+        let equation = this.display.value;
+        try{
+            equation = eval(equation);
+            if(!equation){
                 alert('Invalid calculation');
                 return;
             }
-        },
-
-        clickMethod() {
-            document.addEventListener('click', (e) => {
-                const el = e.target;
-                if (el.classList.contains('btn-num')){
-                    this.btnToDisplay(el.innerText);
-                }
-                if (el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-                if(el.classList.contains('btn-del')){
-                    this.deleteOne();
-                }
-                if(el.classList.contains('btn-eql')){
-                    this.doesEquation()
-                }
-            });
-        },
-
-        btnToDisplay(value){
-            this.display.value += value;
-        },
-    };
+            this.display.value = equation;
+        }
+        catch(e) {
+            alert('Invalid calculation');
+            return;
+        }
+    }
+    const clickMethod = function(){
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+            if (el.classList.contains('btn-num')){
+                btnToDisplay(el.innerText);
+            }
+            if (el.classList.contains('btn-clear')){
+                clearDisplay();
+            }
+            if(el.classList.contains('btn-del')){
+                deleteOne();
+            }
+            if(el.classList.contains('btn-eql')){
+                doesEquation()
+            }
+        });
+    }
 }
 
-const calculator = createCalc();
+const calculator = new Calc(document.querySelector('#display'));
 calculator.initialize();
